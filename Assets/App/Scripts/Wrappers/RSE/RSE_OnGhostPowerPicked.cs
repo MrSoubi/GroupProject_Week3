8207d8dabc6f3ec9c907467e54efcb03f6138452
EmmanuelBouis <emmanuel.bouis@outlook.fr>
2025-03-24 08:43:13 +0100

    Fix Références perdues

---
+++ Assets/App/Scripts/Wrappers/RSE/RSE_OnGhostPowerPicked.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_OnGhostPowerPicked", menuName = "RSE/PowerUp/RSE_OnGhostPowerPicked")]
+public class RSE_OnGhostPowerPicked : RSE_PowerUpPickable { }
--- Assets/App/Scripts/Wrappers/RSE/RSE_PowerUpPickable.cs
+++ Assets/App/Scripts/Wrappers/RSE/RSE_PowerUpPickable.cs
@@ -1,6 +1,3 @@
 using UnityEngine;
 
 public class RSE_PowerUpPickable : BT.ScriptablesObject.RuntimeScriptableEvent<float>{}
-
-[CreateAssetMenu(fileName = "RSE_OnGhostPowerPicked", menuName = "RSE/PowerUp/RSE_OnGhostPowerPicked")]
-public class RSE_OnGhostPowerPicked : RSE_PowerUpPickable {}
